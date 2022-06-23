593702e284c3493f5a5b1eddda073ca36ca5ca12
dakshnitish <dakshnitish@gmail.com>
2022-06-23 23:59:56 +0530

    made a program

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 basics of object oriented programming
 i have done some changes.
-today i wrote a new program.
+today i wrote a new program./n
+i have written something  new today
--- learn.py
+++ learn.py
@@ -1,3 +1,8 @@
-lst=[1,2,3,4,4,5]
-final_set=set(lst)
-print('result:',final_set)
+# lst=[1,2,3,4,4,5]
+# final_set=set(lst)
+# print('result:',final_set)
+
+name = input('enter your name:')
+age = int(input('enter your age:'))
+year = 2022- age + 100
+print(name + ' will turn to 100 years old on '+str(year))
